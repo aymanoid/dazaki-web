0dfa2efa9123ec6a15877244a7447f6fe6972f3e
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-04-11 23:01:52 +0000

    make projects link

--- components/Header.jsx
+++ components/Header.jsx
@@ -135,7 +135,7 @@ const Header = () => {
               href="#"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
-              Partners
+              Projects
             </a>
             <a
               href="#"
