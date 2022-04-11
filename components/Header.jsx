d79da0dd75cc367a952547d33c0373e8c3d73b6c
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-04-11 22:58:22 +0000

    make about link

--- components/Header.jsx
+++ components/Header.jsx
@@ -141,7 +141,7 @@ const Header = () => {
               href="#"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
-              Company
+              About
             </a>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
