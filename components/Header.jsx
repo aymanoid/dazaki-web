130e73bb03ce7e1d57862f0de1f3ed2abe5c4b69
Ayman <56372390+zeutyx@users.noreply.github.com>
2022-04-11 04:10:21 +0000

    make contact button

--- components/Header.jsx
+++ components/Header.jsx
@@ -147,15 +147,9 @@ const Header = () => {
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
             <a
               href="#"
-              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-            >
-              Sign in
-            </a>
-            <a
-              href="#"
               className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
             >
-              Sign up
+              Contact
             </a>
           </div>
         </div>
