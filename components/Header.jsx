f385cabf6d0595174b57fd91fba7d5564f491175
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-05 19:33:48 +0000

    stylize the desktop header

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,6 +11,7 @@ import {
   MicrophoneIcon,
 } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
+import WebsiteLogoSvg from "components/svgs/website_logo.svg";
 
 const menuData = [
   {
@@ -58,16 +59,12 @@ function classNames(...classes) {
 const Header = () => {
   return (
     <header>
-      <Popover className="relative bg-white">
+      <Popover className="relative bg-black">
         <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="#">
               <span className="sr-only">Workflow</span>
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="https://tailwindui.com/img/logos/workflow-mark-purple-600-to-indigo-600.svg"
-                alt=""
-              />
+              <WebsiteLogoSvg className="h-8 w-auto fill-white sm:h-10" />
             </a>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
@@ -82,15 +79,15 @@ const Header = () => {
                 <>
                   <Popover.Button
                     className={classNames(
-                      open ? "text-gray-900" : "text-gray-500",
-                      "group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      open ? "text-rose-500" : "text-white",
+                      "focus:ring-offset-none group inline-flex items-center rounded-md bg-black text-base font-medium hover:text-rose-500 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 focus:ring-offset-black"
                     )}
                   >
                     <span>Industries</span>
                     <ChevronDownIcon
                       className={classNames(
-                        open ? "text-gray-600" : "text-gray-400",
-                        "ml-2 h-5 w-5 group-hover:text-gray-500"
+                        open ? "text-rose-500" : "text-white",
+                        "ml-2 h-5 w-5 group-hover:text-rose-500"
                       )}
                       aria-hidden="true"
                     />
@@ -114,7 +111,7 @@ const Header = () => {
                               href={item.href}
                               className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
                             >
-                              <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white sm:h-12 sm:w-12">
+                              <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-gradient-to-r from-purple-600 to-rose-600 text-white sm:h-12 sm:w-12">
                                 <item.icon
                                   className="h-6 w-6"
                                   aria-hidden="true"
@@ -140,19 +137,19 @@ const Header = () => {
 
             <a
               href="#"
-              className="text-base font-medium text-gray-500 hover:text-gray-900"
+              className="text-base font-medium text-white hover:text-rose-500"
             >
               About
             </a>
             <a
               href="#"
-              className="text-base font-medium text-gray-500 hover:text-gray-900"
+              className="text-base font-medium text-white hover:text-rose-500"
             >
               Work
             </a>
             <a
               href="#"
-              className="text-base font-medium text-gray-500 hover:text-gray-900"
+              className="text-base font-medium text-white hover:text-rose-500"
             >
               Blog
             </a>
@@ -160,7 +157,7 @@ const Header = () => {
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
             <a
               href="#"
-              className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
+              className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-rose-600 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-rose-700"
             >
               Contact
             </a>
@@ -249,6 +246,7 @@ const Header = () => {
             </div>
           </Popover.Panel>
         </Transition>
+        <div className="bg-gradient-to-r from-purple-600 via-violet-600 to-rose-600 p-1"></div>
       </Popover>
     </header>
   );
