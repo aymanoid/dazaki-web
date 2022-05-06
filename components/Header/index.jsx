398e649aa62ea550fc5ed2f59c5dcfe4e226bbc6
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-06 07:30:51 +0000

    stylize open menu button

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -69,7 +69,7 @@ const Header = () => {
             </a>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
-            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-black hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-500">
               <span className="sr-only">Open menu</span>
               <MenuIcon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
