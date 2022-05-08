60ec1cf83d1272d7eb4de8d5457ca3c19fe155e5
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-08 00:54:55 +0000

    add first section

---
+++ components/home/FirstSection.jsx
@@ -0,0 +1,23 @@
+const FirstSection = () => {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-base font-semibold uppercase tracking-wide text-purple-600">
+            Unlimited Possiblities
+          </h2>
+          <p className="mt-1 text-4xl font-extrabold text-black sm:text-5xl sm:tracking-tight lg:text-6xl">
+            It's simpler than you think!
+          </p>
+          <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat
+            scelerisque varius morbi enim.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FirstSection;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
 import Layout from "components/Layout";
 import HeroSection from "components/home/HeroSection";
+import FirstSection from "components/home/FirstSection";
 
 const Home = () => {
   return (
     <Layout>
       <main>
         <HeroSection />
+        <div className="bg-gradient-to-r from-purple-600 via-violet-600 to-rose-600 p-1" />
+        <FirstSection />
       </main>
     </Layout>
   );
