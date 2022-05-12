1ad2295c936577f1116bccd13e1504841e748252
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-05-12 14:04:37 +0100

    add construction banner

---
+++ components/home/ConstrBanner.jsx
@@ -0,0 +1,56 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { ExclamationIcon, XIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const ConstrBanner = () => {
+  const [closed, setClosed] = useState(false);
+
+  return (
+    <div
+      className={classNames(
+        closed ? "hidden" : "",
+        "fixed inset-x-0 bottom-0 pb-2 sm:pb-5"
+      )}
+    >
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="rounded-lg bg-purple-600 p-2 shadow-lg sm:p-3">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex w-0 flex-1 items-center">
+              <span className="flex rounded-lg bg-purple-800 p-2">
+                <ExclamationIcon
+                  className="h-6 w-6 text-white"
+                  aria-hidden="true"
+                />
+              </span>
+              <p className="ml-3 truncate font-medium text-white">
+                <span className="md:hidden">
+                  Our new website is under construction.
+                </span>
+                <span className="hidden md:inline">
+                  Our new website is under construction. Please come back later
+                  for the complete experience!
+                </span>
+              </p>
+            </div>
+            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+              <button
+                onClick={() => setClosed(true)}
+                type="button"
+                className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white"
+              >
+                <span className="sr-only">Dismiss</span>
+                <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConstrBanner;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Layout from "components/Layout";
 import HeroSection from "components/home/HeroSection";
 import FirstSection from "components/home/FirstSection";
 import ServicesSection from "components/home/ServicesSection";
+import ConstrBanner from "components/home/ConstrBanner";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
         <FirstSection />
         <div className="bg-gradient-to-r from-purple-600 via-violet-600 to-rose-600 p-1" />
         <ServicesSection />
+        <ConstrBanner />
       </main>
     </Layout>
   );
