5984950f996fcb3ba9c3b102e62fb8d68ecb8bd2
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-09-04 20:35:51 +0100

    update services section

--- components/home/ServicesSection.jsx
+++ components/home/ServicesSection.jsx
@@ -1,33 +1,29 @@
+import Image from "next/image";
+
 const servicesData = [
   {
-    name: "Web Development",
+    title: "Web App Development",
     description:
       "Complete website development and design, from UX design to front-end coding to custom databases.",
+    imageUrl: "/assets/services-01.png",
   },
   {
-    name: "eCommerce Solutions",
-    description:
-      "Ecommerce store development and setup, including payment integration, and custom merch supplying.",
-  },
-  {
-    name: "Social Media Marketing",
-    description:
-      "Creating and managing social media ad campaigns, A/B testing and optimizing compaigns based on analytics, and more.",
-  },
-  {
-    name: "Digital Branding",
-    description:
-      "Brand indentity development, providing corporate or product name research, design elements, and brand guidelines.",
-  },
-  {
-    name: "Mobile App Development",
-    description:
-      "Fully-featured native mobile application development and design, for both Android and iOS.",
-  },
-  {
-    name: "Custom Software Development",
+    title: "Custom Sotfware Development",
     description:
       "For uncommon problems, we provide custom software solutions, such as desktop apps, server-only utilities, and chat bots.",
+    imageUrl: "/assets/services-02.png",
+  },
+  {
+    title: "eCommerce Solutions",
+    description:
+      "Ecommerce store development and setup, including payment integration, and custom merch supplying.",
+    imageUrl: "/assets/services-03.png",
+  },
+  {
+    title: "Mobile App Development",
+    description:
+      "Fully-featured native mobile application development and design, for both Android and iOS.",
+    imageUrl: "/assets/services-04.png",
   },
 ];
 
@@ -35,31 +31,31 @@ const ServicesSection = () => {
   return (
     <section className="relative bg-zinc-900 py-16 sm:py-24 lg:py-32">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-        <p className="mt-2 text-left text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
+        <h2 className="mt-2 text-left text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
           Some of the neat services we offer
-        </p>
+        </h2>
         <p className="mt-5 max-w-prose text-left text-xl text-gray-300">
           From simple web fronts, to full-fledged apps and ecommerce projects,
           we offer a wide range of services to our individual and business
           clients.
         </p>
-        <div className="mt-12">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            {servicesData.map((e) => (
-              <div key={e.name} className="pt-6">
-                <div className="rounded-lg bg-gradient-to-tr from-purple-500 to-rose-700 p-1">
-                  <div className="flow-root h-48 rounded-lg bg-zinc-900 px-6 pb-8">
-                    <h3 className="mt-8 text-lg font-semibold tracking-tight text-white">
-                      {e.name}
-                    </h3>
-                    <p className="mt-5 text-base text-gray-300">
-                      {e.description}
-                    </p>
-                  </div>
-                </div>
+        <div className="mt-12 grid grid-cols-1 gap-16 sm:grid-cols-2 ">
+          {servicesData.map((e) => (
+            <div key={e.title} className="space-y-4">
+              <Image
+                src={e.imageUrl}
+                alt={`${e.title} icon`}
+                width={1920}
+                height={1080}
+                quality={80}
+                className="rounded-lg"
+              />
+              <div className="space-y-2">
+                <h3 className="text-xl text-white font-semibold">{e.title}</h3>
+                <p className="text-left text-gray-300">{e.description}</p>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
