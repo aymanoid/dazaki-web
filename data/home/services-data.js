6aa55c054f884d0037efff73329272ece11e93b7
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-10-04 00:14:59 +0100

    extract services section data

---
+++ data/home/services-data.js
@@ -0,0 +1,28 @@
+const servicesData = [
+  {
+    title: "Web Apps",
+    description:
+      "Complete website development and design, from UX design to front-end coding to custom databases.",
+    imageUrl: "/assets/services-01.png",
+  },
+  {
+    title: "Custom Software",
+    description:
+      "For uncommon problems, we provide custom software solutions, such as desktop apps, server-only utilities, and chat bots.",
+    imageUrl: "/assets/services-02.png",
+  },
+  {
+    title: "eCommerce Solutions",
+    description:
+      "Ecommerce store development and setup, including payment integration, and custom merch supplying.",
+    imageUrl: "/assets/services-03.png",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Fully-featured native mobile application development and design, for both Android and iOS.",
+    imageUrl: "/assets/services-04.png",
+  },
+];
+
+export default servicesData;
