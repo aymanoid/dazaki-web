1df6bb87d7d050cfd19ab68fe40661dc728c01ff
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-10-17 18:53:25 +0100

    increase boldness of last section button

--- src/components/home/LastSection.jsx
+++ src/components/home/LastSection.jsx
@@ -13,7 +13,7 @@ const LastSection = () => {
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
             <Link href="/contact">
-              <a className="inline-flex items-center justify-center rounded-xl border border-transparent bg-black px-5 py-3 text-base font-medium text-white hover:bg-white hover:text-black">
+              <a className="inline-flex items-center justify-center rounded-xl border border-transparent bg-black px-5 py-3 text-base font-semibold text-white hover:bg-white hover:text-black">
                 Contact Us
               </a>
             </Link>
