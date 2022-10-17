86f7e71a1e13dbedfc5cdf41ff81bec6ba89e83d
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-10-17 18:13:46 +0100

    implement features section

---
+++ src/components/home/FeaturesSection.jsx
@@ -0,0 +1,44 @@
+import featuresData from "@/data/home/features-data";
+
+const FeaturesSection = () => {
+  return (
+    <section className="bg-white py-16">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base font-semibold uppercase tracking-wide text-purple-600">
+            Why choose us
+          </h2>
+          <p className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            Features that we provide
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            There are many factors that set us apart from the rest, and these
+            are just some of them. 😎
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
+            {featuresData.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-gradient-to-r from-rose-500 to-purple-600 text-white">
+                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
+                    {feature.name}
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturesSection;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import ConstrBanner from "@/components/home/ConstrBanner";
 import FirstSection from "@/components/home/FirstSection";
 import HeroSection from "@/components/home/HeroSection";
 import ServicesSection from "@/components/home/ServicesSection";
+import FeaturesSection from "@/components/home/FeaturesSection";
 import { NextSeo } from "next-seo";
 
 const Home = () => {
@@ -18,6 +19,8 @@ const Home = () => {
         <FirstSection />
         <div className="bg-gradient-to-r from-purple-600 via-violet-600 to-rose-600 p-1" />
         <ServicesSection />
+        <div className="bg-gradient-to-r from-purple-600 via-violet-600 to-rose-600 p-1" />
+        <FeaturesSection />
         <ConstrBanner />
       </Layout>
     </>
