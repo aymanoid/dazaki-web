7303eb5cb573791deb40efc46ca22c540d10ed15
Ayman <56372390+aymanoid@users.noreply.github.com>
2022-10-17 18:51:12 +0100

    implement last section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import FirstSection from "@/components/home/FirstSection";
 import HeroSection from "@/components/home/HeroSection";
 import ServicesSection from "@/components/home/ServicesSection";
 import FeaturesSection from "@/components/home/FeaturesSection";
+import LastSection from "@/components/home/LastSection";
 import { NextSeo } from "next-seo";
 
 const Home = () => {
@@ -21,6 +22,8 @@ const Home = () => {
         <ServicesSection />
         <div className="bg-gradient-to-r from-purple-600 via-violet-600 to-rose-600 p-1" />
         <FeaturesSection />
+        <div className="bg-gradient-to-r from-purple-600 via-violet-600 to-rose-600 p-1" />
+        <LastSection />
         <ConstrBanner />
       </Layout>
     </>
