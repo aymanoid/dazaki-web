bc9ecd4afafd0eaa2d6ba5fa326a6d44a7b12154
Ayman <56372390+aymanoid@users.noreply.github.com>
2024-05-04 21:51:27 +0100

    use static export instead of cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,23 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import Icons from "unplugin-icons/vite";
 import { FileSystemIconLoader } from "unplugin-icons/loaders";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
+  output: "static",
   integrations: [react(), tailwind()],
   vite: {
-    plugins: [Icons({
-      compiler: "jsx",
-      jsx: "react",
-      customCollections: {
-        "my-icons": FileSystemIconLoader("./src/assets/icons", svg => svg.replace(/^<svg /, '<svg fill="currentColor" '))
-      }
-    })]
-  }
+    plugins: [
+      Icons({
+        compiler: "jsx",
+        jsx: "react",
+        customCollections: {
+          "my-icons": FileSystemIconLoader("./src/assets/icons", (svg) =>
+            svg.replace(/^<svg /, '<svg fill="currentColor" '),
+          ),
+        },
+      }),
+    ],
+  },
 });
