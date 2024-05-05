9595bb6d4b7d6491a29a3c64820831d0c0b01833
Ayman <56372390+aymanoid@users.noreply.github.com>
2024-05-05 01:13:41 +0100

    add index page

---
+++ src/data/home/features-data.ts
@@ -0,0 +1,35 @@
+import LightBulbIcon from "~icons/heroicons/light-bulb";
+import PaintBrushIcon from "~icons/heroicons/paint-brush";
+import BoltIcon from "~icons/heroicons/bolt";
+import WrenchScrewdriverIcon from "~icons/heroicons/wrench-screwdriver";
+
+const featuresData = [
+  {
+    name: "Innovative solutions",
+    description:
+      "There are no technical limitations to what our applications can do. Just tell us your idea and we'll make sure to find a way to implement it in the best way possible.",
+    icon: LightBulbIcon,
+  },
+  {
+    name: "Modern designs",
+    description:
+      "Our websites and applications feature visually appealing and responsive designs that look and work well on all modern devices, designed with accessibility in mind.",
+    icon: PaintBrushIcon,
+  },
+  {
+    name: "Cutting-edge technologies",
+    description:
+      "We utilize industry standard methods and technologies while building our applications to ensure achiving top notch perfomance and scalability.",
+    icon: BoltIcon,
+  },
+  {
+    name: "Continuous support",
+    description:
+      "Our service does not end when we deliver the project. Instead, we continue to provide updates and maintenance as necessary.",
+    icon: WrenchScrewdriverIcon,
+  },
+];
+
+//    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+
+export default featuresData;
---
+++ src/data/home/services-data.ts
@@ -0,0 +1,33 @@
+import service01Image from "@/assets/images/services-01.png";
+import service02Image from "@/assets/images/services-02.png";
+import service03Image from "@/assets/images/services-03.png";
+import service04Image from "@/assets/images/services-04.png";
+
+const servicesData = [
+  {
+    title: "Web Apps",
+    description:
+      "Complete website development and design, from UX design to front-end coding to custom databases.",
+    image: service01Image,
+  },
+  {
+    title: "Custom Software",
+    description:
+      "For uncommon problems, we provide custom software solutions, such as desktop apps, server-only utilities, and chat bots.",
+    image: service02Image,
+  },
+  {
+    title: "eCommerce Solutions",
+    description:
+      "Ecommerce store development and setup, including payment integration, and custom merch supplying.",
+    image: service03Image,
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Fully-featured native mobile application development and design, for both Android and iOS.",
+    image: service04Image,
+  },
+];
+
+export default servicesData;
