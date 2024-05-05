ef6cb2fa26876ee8f9146f839a34c8d90ae08d36
Ayman <56372390+aymanoid@users.noreply.github.com>
2024-05-05 02:43:44 +0100

    add contact page

---
+++ src/components/contact/ContactInfo.tsx
@@ -0,0 +1,69 @@
+import EmailIcon from "~icons/my-icons/iconmonstr-email-1.svg";
+import DiscordIcon from "~icons/my-icons/discord.svg";
+import TelegramIcon from "~icons/my-icons/telegram.svg";
+
+const contactMethods = [
+  {
+    name: "Email",
+    username: "ayman@dazaki.com",
+    href: "mailto:ayman@dazaki.com",
+    icon: EmailIcon,
+  },
+  {
+    name: "Discord",
+    username: "aymanoid#0609",
+    href: "https://discord.com/users/714385023112970303",
+    icon: DiscordIcon,
+  },
+  {
+    name: "Telegram",
+    username: "aymanoid",
+    href: "https://t.me/aymanoid",
+    icon: TelegramIcon,
+  },
+];
+
+const ContactInfo = () => {
+  return (
+    <div className="bg-white px-4 py-16 sm:px-6 lg:col-span-2 lg:px-8 lg:py-28 xl:pr-12">
+      <div className="mx-auto max-w-lg">
+        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+          Get in touch
+        </h2>
+        <p className="mt-3 text-lg leading-6 text-gray-500">
+          If you&apos;d like to discuss a project idea, general inquiries, or
+          anything else, feel free to get in touch.
+        </p>
+        <dl className="mt-8 text-base text-gray-500">
+          <div>
+            <dt className="sr-only">Contact Method</dt>
+            <dd>
+              <p>Use the form or your preferred contact method to reach out.</p>
+            </dd>
+          </div>
+          {contactMethods.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="mt-3 hover:text-rose-600">
+                <dt className="sr-only">{item.name}</dt>
+                <dd className="flex">
+                  <item.icon
+                    className="h-6 w-6 flex-shrink-0 fill-gray-400"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-3">{item.username}</span>
+                </dd>
+              </div>
+            </a>
+          ))}
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
---
+++ src/components/contact/SubmissionBanner.tsx
@@ -0,0 +1,102 @@
+import ExclamationTriangleIcon from "~icons/heroicons/exclamation-triangle";
+import XMarkIcon from "~icons/heroicons/x-mark";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+const statusMessages = {
+  "0": {
+    short: "We will get back to you soon!",
+    long: "We've received your message and will get back to you soon!",
+  },
+  "1": {
+    short: "Name field is required.",
+    long: "Name field is required.",
+  },
+  "2": {
+    short: "Email field is required.",
+    long: "Email field is required.",
+  },
+  "3": {
+    short: "Subject field is required.",
+    long: "Subject field is required.",
+  },
+  "4": {
+    short: "Message field is required.",
+    long: "Message field is required.",
+  },
+  "5": {
+    short: "Name is 256 characters max.",
+    long: "Name must be no more than 256 characters in length.",
+  },
+  "6": {
+    short: "Email is 256 characters max.",
+    long: "Email must be no more than 256 characters in length.",
+  },
+  "7": {
+    short: "Subject is 256 characters max.",
+    long: "Subject must be no more than 256 characters in length.",
+  },
+  "8": {
+    short: "Message is 256 characters max.",
+    long: "Message must be no more than 256 characters in length.",
+  },
+  "9": {
+    short: "An error occurred while sending.",
+    long: "An error occurred while sending your message, please try again.",
+  },
+};
+
+interface SubmissionBannerProps {
+  statusCode: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+}
+
+const SubmissionBanner = ({ statusCode }: SubmissionBannerProps) => {
+  const [isBannerOpen, setIsBannerOpen] = useState(true);
+
+  return (
+    <div
+      className={cn(
+        isBannerOpen ? "" : "hidden",
+        "fixed inset-x-0 bottom-0 pb-2 sm:pb-5",
+      )}
+    >
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="rounded-lg bg-purple-600 p-2 shadow-lg sm:p-3">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex w-0 flex-1 items-center">
+              <span className="flex rounded-lg bg-purple-800 p-2">
+                <ExclamationTriangleIcon
+                  className="h-6 w-6 text-white"
+                  aria-hidden="true"
+                />
+              </span>
+              <p className="ml-3 truncate font-semibold text-white">
+                <span className="md:hidden">
+                  {statusMessages[statusCode].short}
+                </span>
+                <span className="hidden md:inline">
+                  {statusMessages[statusCode].long}
+                </span>
+              </p>
+            </div>
+            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+              <button
+                onClick={() => {
+                  // router.replace("/contact");
+                  setIsBannerOpen(false);
+                }}
+                type="button"
+                className="-mr-1 flex rounded-md p-2 hover:bg-rose-500 focus:outline-none focus:ring-2 focus:ring-white"
+              >
+                <span className="sr-only">Dismiss</span>
+                <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubmissionBanner;
