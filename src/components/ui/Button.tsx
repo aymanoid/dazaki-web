848ead3daf85a83dc8f6fa801d2783a5292bb309
Ayman <56372390+aymanoid@users.noreply.github.com>
2024-05-06 12:24:17 +0100

    redesign hero section

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,67 @@
+import type {
+  ButtonHTMLAttributes,
+  AnchorHTMLAttributes,
+  FC,
+  ForwardedRef,
+} from "react";
+import { forwardRef } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-2xl border border-transparent font-semibold text-white shadow-sm ",
+  {
+    variants: {
+      variant: {
+        branded:
+          "bg-gradient-to-r from-purple-600 to-rose-600 bg-origin-border hover:from-purple-500 hover:to-rose-500",
+      },
+      size: {
+        md: "text-base px-4 py-2",
+        lg: "text-base px-8 py-3 md:px-10 md:py-4 md:text-lg",
+      },
+    },
+    // defaultVariants: {
+    //   variant: "default",
+    //   size: "md",
+    // },
+  },
+);
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button: FC<ButtonProps> = forwardRef(
+  (
+    { className, variant, size, ...props },
+    ref: ForwardedRef<HTMLButtonElement>,
+  ) => (
+    <button
+      ref={ref}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  ),
+);
+
+export default Button;
+
+export interface LinkButtonProps
+  extends AnchorHTMLAttributes<HTMLAnchorElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const LinkButton: FC<LinkButtonProps> = forwardRef(
+  (
+    { className, variant, size, ...props },
+    ref: ForwardedRef<HTMLAnchorElement>,
+  ) => (
+    <a
+      ref={ref}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  ),
+);
+
+export { LinkButton };
